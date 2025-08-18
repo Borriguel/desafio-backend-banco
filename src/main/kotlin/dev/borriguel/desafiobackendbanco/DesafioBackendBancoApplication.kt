5f3d30d841b28f1643926a68a96ba9966c621223
Borriguel <rodolpho-henrique@hotmail.com.br>
2025-08-18 20:50:19 -0300

    Commit inicial

---
+++ src/main/kotlin/dev/borriguel/desafiobackendbanco/DesafioBackendBancoApplication.kt
@@ -0,0 +1,11 @@
+package dev.borriguel.desafiobackendbanco
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DesafioBackendBancoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DesafioBackendBancoApplication>(*args)
+}
---
+++ src/test/kotlin/dev/borriguel/desafiobackendbanco/DesafioBackendBancoApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.borriguel.desafiobackendbanco
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DesafioBackendBancoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
