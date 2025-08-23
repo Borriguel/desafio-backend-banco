4ff9c4cd96071ec7aa1220068290f8b25b4b3b86
Borriguel <rodolpho-henrique@hotmail.com.br>
2025-08-23 18:10:52 -0300

    Adiciona `WalletService` com operações para criação, consulta, depósito e saque de carteiras.

---
+++ src/main/kotlin/dev/borriguel/desafiobackendbanco/service/WalletService.kt
@@ -0,0 +1,35 @@
+package dev.borriguel.desafiobackendbanco.service
+
+import dev.borriguel.desafiobackendbanco.model.AccountType
+import dev.borriguel.desafiobackendbanco.model.Wallet
+import dev.borriguel.desafiobackendbanco.repository.WalletRepository
+import org.springframework.stereotype.Service
+import java.math.BigDecimal
+
+@Service
+class WalletService(private val repository: WalletRepository) {
+    suspend fun createWallet(accountType: AccountType) : Wallet {
+        return repository.save(Wallet(
+            type = accountType
+        ))
+    }
+
+    suspend fun getById(id: Long) : Wallet {
+        return repository.findById(id) ?: throw IllegalArgumentException("Wallet not found")
+    }
+
+    suspend fun depositById(id: Long, amount: BigDecimal) : Wallet {
+        val wallet = getById(id)
+        wallet.deposit(amount)
+        repository.save(wallet)
+        return wallet
+    }
+
+    suspend fun withdrawById(id: Long, amount: BigDecimal) : Wallet {
+        val wallet = getById(id)
+        wallet.withdraw(amount)
+        repository.save(wallet)
+        return wallet
+    }
+
+}
