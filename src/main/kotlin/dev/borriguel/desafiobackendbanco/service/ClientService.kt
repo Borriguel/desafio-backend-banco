6fa6a7295f7d97a6b45b2da885b2ea92e9114fbd
Borriguel <rodolpho-henrique@hotmail.com.br>
2025-08-23 16:57:25 -0300

    Adiciona `ClientService` com operações CRUD e integração com `WalletService`.

---
+++ src/main/kotlin/dev/borriguel/desafiobackendbanco/service/ClientService.kt
@@ -0,0 +1,50 @@
+package dev.borriguel.desafiobackendbanco.service
+
+import dev.borriguel.desafiobackendbanco.dto.ClientDto
+import dev.borriguel.desafiobackendbanco.model.AccountType
+import dev.borriguel.desafiobackendbanco.model.Client
+import dev.borriguel.desafiobackendbanco.repository.ClientRepository
+import kotlinx.coroutines.flow.toList
+import org.springframework.stereotype.Service
+
+@Service
+class ClientService(private val repository: ClientRepository, private val walletService: WalletService) {
+    suspend fun createClient(email: String, password: String, accountType: AccountType) : Client {
+        if (existsByEmail(email)) throw IllegalArgumentException("Client already exists")
+        val wallet = walletService.createWallet(accountType)
+        val client = Client(
+            email = email, password = password,
+            walletId = wallet.id ?: throw IllegalStateException("Invalid wallet ID")
+        )
+        return repository.save(client)
+    }
+
+    suspend fun getById(id: Long): Client? {
+        return repository.findById(id) ?: throw IllegalArgumentException("Client not found")
+    }
+
+    suspend fun getByEmail(email: String): Client? {
+        return repository.findByEmail(email) ?: throw IllegalArgumentException("Client not found")
+    }
+
+    private suspend fun existsByEmail(email: String): Boolean {
+        return repository.existsByEmail(email)
+    }
+
+    suspend fun deleteById(id: Long) {
+        getById(id)
+        repository.deleteById(id)
+    }
+
+    suspend fun getAll(): List<Client> {
+        return repository.findAll().toList()
+    }
+
+    suspend fun updateById(id: Long, clientUpdate: ClientDto): Client {
+        val existingClient = getById(id) ?: throw IllegalArgumentException("Client not found")
+        existingClient.changeEmail(clientUpdate.email)
+        existingClient.changePassword(clientUpdate.password)
+        return repository.save(existingClient)
+    }
+
+}
