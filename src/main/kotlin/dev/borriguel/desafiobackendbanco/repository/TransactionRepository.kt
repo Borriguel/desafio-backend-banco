dd6fae9ab243dc419a6cff4e511330bb238895eb
Borriguel <rodolpho-henrique@hotmail.com.br>
2025-08-26 21:03:08 -0300

    Altera método `existsByIdempotencyKey` para `findByIdempotencyKey` na interface `TransactionRepository` e ajusta o retorno para `Transaction?`.

--- src/main/kotlin/dev/borriguel/desafiobackendbanco/repository/TransactionRepository.kt
+++ src/main/kotlin/dev/borriguel/desafiobackendbanco/repository/TransactionRepository.kt
@@ -1,7 +1,10 @@
 package dev.borriguel.desafiobackendbanco.repository
 
+import dev.borriguel.desafiobackendbanco.model.Status
 import dev.borriguel.desafiobackendbanco.model.Transaction
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 
 interface TransactionRepository: CoroutineCrudRepository<Transaction, Long> {
+    suspend fun findByStatus(status: Status): List<Transaction>
+    suspend fun findByIdempotencyKey(idempotencyKey: String): Transaction?
 }
