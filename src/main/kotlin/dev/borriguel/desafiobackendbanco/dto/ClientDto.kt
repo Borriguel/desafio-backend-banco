c317c6c063066676f8d38664967fbb04e22b92d1
Borriguel <rodolpho-henrique@hotmail.com.br>
2025-08-28 20:37:47 -0300

    Adiciona DTO `ClientWalletDto` e validações para atributos `name` e `document` em `ClientDto`.

--- src/main/kotlin/dev/borriguel/desafiobackendbanco/dto/ClientDto.kt
+++ src/main/kotlin/dev/borriguel/desafiobackendbanco/dto/ClientDto.kt
@@ -1,6 +1,7 @@
 package dev.borriguel.desafiobackendbanco.dto
 
 import dev.borriguel.desafiobackendbanco.model.AccountType
+import dev.borriguel.desafiobackendbanco.model.Wallet
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
@@ -10,11 +11,26 @@ data class ClientDto(
     @field:NotBlank(message = "Email cannot be blank")
     @field:Email(message = "Email should be valid")
     val email: String,
-    
+
     @field:NotBlank(message = "Password cannot be blank")
     @field:Size(min = 6, message = "Password must be at least 6 characters long")
     val password: String,
-    
+
+    @field:NotBlank(message = "Name cannot be blank")
+    @field:Size(max = 100, message = "Name must be at most 100 characters long")
+    val name: String,
+
+    @field:NotBlank(message = "Document cannot be blank")
+    @field:Size(min = 11, max = 11, message = "Document must be 11 characters long")
+    val document: String,
+
     @field:NotNull(message = "Account type cannot be null")
     val accountType: AccountType
+)
+
+data class ClientWalletDto(
+    val id: String,
+    val name: String,
+    val document: String,
+    val wallet: Wallet
 )
